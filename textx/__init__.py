f78cdf901709aabc4642fc81fc7462879fd381b8
Vincent Aranega <vincent.aranega@axellience.com>
2017-10-08 23:06:25 +0200

    Change way of switching from a mode to another

--- textx/__init__.py
+++ textx/__init__.py
@@ -7,6 +7,19 @@ def is_pyecore_enabled():
     return __PYECORE_SUPPORT
 
 
-def enable_pyecore_support(enabled=True):
+def enable_pyecore_support(enable=True):
     global __PYECORE_SUPPORT
-    __PYECORE_SUPPORT = enabled
+    __PYECORE_SUPPORT = enable
+    import sys
+    if sys.version > '3':
+        try:
+            from importlib import reload
+        except ImportError:
+            from imp import reload
+
+    import textx.model
+    import textx.metamodel
+    import textx.textx
+    reload(textx.metamodel)
+    reload(textx.model)
+    reload(textx.textx)
